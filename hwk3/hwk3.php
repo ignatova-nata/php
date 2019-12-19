7346c53c774986d1b59c6cf5c1f84b7d495dfadf
ignatova-nata <ignatova_nata@inbox.ru>
2019-12-19 15:18:13 +0300

    1-5

--- hwk3/hwk3.php
+++ hwk3/hwk3.php
@@ -95,3 +95,47 @@ switch ($percAver) {
     return $rating;
   }
   var_dump("Задание 3. ",textCompare($text11, $text22));
+
+  /* Дан массив, состоящий из целых чисел. Написать функцию сортировки массива
+  по возрастанию суммы цифр чисел. Например, дан массив [13, 55, 100].
+  После сортировки он будет следующего вида: [100, 13, 55],
+   тк сумма цифр числа 100 = 1, сумма цифр числа 13 = 4, а 55 = 10.
+   На экран вывести исходный массив, массив после сортировки
+   и сумму цифр каждого числа отсортированного массива.*/
+
+   $arr1 = [13, 55, 100];
+   function sortArr($arr) {
+     for ($i=0; $i < count($arr); $i++) {
+     $arrSum[] = [$arr[$i],array_sum(str_split($arr[$i]))];
+     }
+     $numb = array_column($arrSum, '0');
+     $sum = array_column($arrSum, '1');
+     array_multisort($sum,SORT_ASC,$arrSum);
+     return $arrSum;
+   }
+var_dump("Задание 4. ", $arr1,array_column(sortArr($arr1),'0'),array_column(sortArr($arr1),'1'));
+
+/*Написать функцию - конвертер строки
+(функция принимает на вход строку и функцию-преобразователь,
+ возвращает преобразованную строку).
+ Использовать анонимные функции. Возможности:
+перевод всех символов в верхний регистр,
+перевод всех символов в нижний регистр,
+перевод всех символов в нижний регистр
+и первых символов слов в верхний регистр.*/
+$string1 = "when Harry met Sally";
+function stringConvert (string $string, callable $func):string {
+                       return $new_string = $func($string);
+                     }
+$stringUp = function ($string) {
+    return strtoupper($string);
+    };
+$stringDown = function ($string) {
+    return strtolower($string);
+    };
+$stringUpDown = function ($string) {
+    return ucwords($string);
+    };
+var_dump("Задание 5.", stringConvert($string1, $stringUp));
+var_dump(stringConvert($string1, $stringDown));
+var_dump(stringConvert($string1, $stringUpDown));
